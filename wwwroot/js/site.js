a76400b5aea650f12935c6687ee4611401c70c41
Diniz Ribeiro <84927857+barrosjunior26@users.noreply.github.com>
2024-11-19 10:07:14 -0300

    Alterações no arquivo site.js para inclusão de condicionais para medir o desempenho do aluno de acordo com a média do mesmo. Alterações em alguns elementos html e bootstrap.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -4,11 +4,33 @@ function calculoMedia() {
     var EstrutDados = parseFloat(window.document.getElementById('EstrutDados').value) || 0;
     var POO = parseFloat(window.document.getElementById('POO').value) || 0;
     var Banco_de_Dados = parseFloat(window.document.getElementById('Banco_de_Dados').value) || 0;
+    const desempenhoLabel = window.document.getElementById('result');
 
     var media = (logica + EstrutDados + POO + Banco_de_Dados) / 4;
 
+    if (media >= 1 && media <= 6.99) {
+        desempenhoLabel.textContent = "Baixo desempenho!";
+        desempenhoLabel.style.color = "red";
+        desempenhoLabel.style.fontWeight = "600";
+    } else if (media >= 7 && media <= 8.99) {
+        desempenhoLabel.textContent = "Ótimo desempenho!";
+        desempenhoLabel.style.color = "#ff9800";
+        desempenhoLabel.style.fontWeight = "600";
+    } else if (media >= 9) {
+        desempenhoLabel.textContent = "Excelente desempenho!";
+        desempenhoLabel.style.color = "green";
+        desempenhoLabel.style.fontWeight = "600";
+    } else {
+        desempenhoLabel.textContent = "Aguardando informações...";
+        desempenhoLabel.style.color = "#4fa8fb";
+        desempenhoLabel.style.fontWeight = "600";
+    }
+
     window.document.getElementById('media').value = media.toFixed(2);
 };
+
+// Verificar o desempenho quando a página é carregada
+calculoMedia();
 
 //Adicionando eventos aos inputs
 window.document.addEventListener('DOMContentLoaded', (event) => {
