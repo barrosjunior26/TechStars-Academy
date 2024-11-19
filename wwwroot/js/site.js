64ce19e57e266dc7620fa94f409b88d445b6b07e
Diniz Ribeiro <84927857+barrosjunior26@users.noreply.github.com>
2024-11-19 07:42:38 -0300

    Inclusão de mais linhas código no arquivo site.js para dar interatividade no cálculo da média do aluno. Edição na View CadastroAluno para inclusão de mais alguns labels e inputs faltantes e inclusão de algumas classes Bootstrap.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,14 +2,18 @@
 function calculoMedia() {
     var logica = parseFloat(window.document.getElementById('logica').value) || 0;
     var EstrutDados = parseFloat(window.document.getElementById('EstrutDados').value) || 0;
+    var POO = parseFloat(window.document.getElementById('POO').value) || 0;
+    var Banco_de_Dados = parseFloat(window.document.getElementById('Banco_de_Dados').value) || 0;
 
-    var media = (logica + EstrutDados) / 2;
+    var media = (logica + EstrutDados + POO + Banco_de_Dados) / 4;
 
-    window.document.getElementById('media').value = media;
+    window.document.getElementById('media').value = media.toFixed(2);
 };
 
 //Adicionando eventos aos inputs
 window.document.addEventListener('DOMContentLoaded', (event) => {
     window.document.getElementById('logica').addEventListener('blur', calculoMedia);
     window.document.getElementById('EstrutDados').addEventListener('blur', calculoMedia);
+    window.document.getElementById('POO').addEventListener('blur', calculoMedia);
+    window.document.getElementById('Banco_de_Dados').addEventListener('blur', calculoMedia);
 });
